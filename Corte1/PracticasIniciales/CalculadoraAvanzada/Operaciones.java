54380bddd9e57db36ad8d63a7fab8f8730fb0085
Luis-17-h <lmhidalgoc@udistrital.edu.co>
2025-10-22 22:51:39 -0500

    Add files via upload

---
+++ Corte1/PracticasIniciales/CalculadoraAvanzada/Operaciones.java
@@ -0,0 +1,43 @@
+public class Operaciones {
+
+    //gets-sets
+    private double numero1;
+    private double numero2;
+
+    public double getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(double numero1) {
+        this.numero1 = numero1;
+    }
+
+    public double getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(double numero2) {
+        this.numero2 = numero2;
+    }
+
+    // Métodos de operaciones
+    public double sumar() {
+        return numero1 + numero2;
+    }
+
+    public double restar() {
+        return numero1 - numero2;
+    }
+
+    public double multiplicar() {
+        return numero1 * numero2;
+    }
+
+    public double dividir() {
+        if (numero2 == 0) {
+            System.out.println("Error: No se puede dividir entre cero.");
+            return 0;
+        }
+        return numero1 / numero2;
+    }
+}
